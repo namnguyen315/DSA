4eec4e2623dc1450906806fee1de19ae562acf1a
namnguyen315 <namnguyen3152001xtnd@gmail.com>
2024-08-07 16:51:36 +0700

    update stacks&queues/queuesUsingArrays.js: feature enqueue dequeue peek size isEmpty print

--- stacks&queues/queuesUsingArrays.js
+++ stacks&queues/queuesUsingArrays.js
@@ -7,6 +7,25 @@ class QueueUsingArray {
     this.queue.push(data);
   }
 
+  dequeue() {
+    this.queue.shift();
+  }
+
+  isEmpty() {
+    if (this.queue.length === 0) {
+      console.log(true);
+    } else console.log(false);
+  }
+  peek() {
+    if (this.queue.length === 0) {
+      console.log("Queue is empty");
+    } else console.log(this.queue[0]);
+  }
+
+  size() {
+    console.log(this.queue.length);
+  }
+
   print() {
     console.log(this.queue);
   }
@@ -16,6 +35,15 @@ const queue = new QueueUsingArray();
 
 queue.enqueue(10);
 queue.enqueue(20);
+queue.enqueue(30);
 queue.enqueue(5);
+
+queue.dequeue();
+
+queue.peek();
+
+queue.size();
+
+queue.isEmpty();
 
 queue.print();
